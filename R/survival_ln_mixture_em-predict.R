4b7ef9047a94f72ff466bbfff7403af530a8bceb
Victor Ney <victor.s.ney@gmail.com>
2024-05-06 10:04:32 -0300

    Criando vignette para o EM e melhorando o predict deste método

--- R/survival_ln_mixture_em-predict.R
+++ R/survival_ln_mixture_em-predict.R
@@ -99,7 +99,13 @@ extract_surv_haz_em <- function(model, predictors, eval_time, type = "survival")
           t <- eval_time[i]
           out_r$.pred_survival[i] <- sob_lognormal_em_mix(t, m[r, ], sigma, eta)
         }
-
+        
+        tib_predictors <- tibble::as_tibble(matrix(rep(predictors[r, ], times = length(eval_time)), ncol = 2, byrow = TRUE), .name_repair = 'minimal')
+        
+        names(tib_predictors) <- colnames(predictors)
+        out_r <- dplyr::bind_cols(out_r,
+                                  tib_predictors)
+        
         out[[r]] <- out_r
       }
     } else {
@@ -112,6 +118,12 @@ extract_surv_haz_em <- function(model, predictors, eval_time, type = "survival")
         t <- eval_time[i]
         out_r$.pred_survival[i] <- sob_lognormal_em_mix(t, m, sigma, eta)
       }
+      
+      tib_predictors <- tibble::as_tibble(matrix(rep(predictors[1, ], times = length(eval_time)), ncol = 2, byrow = TRUE), .name_repair = 'minimal')
+      
+      names(tib_predictors) <- colnames(predictors)
+      out_r <- dplyr::bind_cols(out_r,
+                                tib_predictors)
 
       out[[1]] <- out_r
     }
@@ -129,6 +141,12 @@ extract_surv_haz_em <- function(model, predictors, eval_time, type = "survival")
           out_r$.pred_hazard[i] <- falha_lognormal_em_mix(t, m[r, ], sigma, eta)
         }
 
+        tib_predictors <- tibble::as_tibble(matrix(rep(predictors[r, ], times = length(eval_time)), ncol = 2, byrow = TRUE), .name_repair = 'minimal')
+        
+        names(tib_predictors) <- colnames(predictors)
+        out_r <- dplyr::bind_cols(out_r,
+                                  tib_predictors)
+        
         out[[r]] <- out_r
       }
     } else {
@@ -142,6 +160,12 @@ extract_surv_haz_em <- function(model, predictors, eval_time, type = "survival")
         out_r$.pred_hazard[i] <- falha_lognormal_em_mix(t, m, sigma, eta)
       }
 
+      tib_predictors <- tibble::as_tibble(matrix(rep(predictors[1, ], times = length(eval_time)), ncol = 2, byrow = TRUE), .name_repair = 'minimal')
+      
+      names(tib_predictors) <- colnames(predictors)
+      out_r <- dplyr::bind_cols(out_r,
+                                tib_predictors)
+      
       out[[1]] <- out_r
     }
   }
