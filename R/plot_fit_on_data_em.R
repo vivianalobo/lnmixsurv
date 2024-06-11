5a33d9452487b49b56a347f2d86d6e562b5c4295
Victor Ney <victor.s.ney@gmail.com>
2024-06-11 15:03:02 -0300

    O predict da função EM foi alterado para prover uma coluna strata compatível com a coluna strata do survival::survift. A função plot_fit_on_data_em foi adaptada para trabalhar com esse tipo de predict. A ideia agora é criar uma função plot_fit_on_data que unifique tanto a visualização para quando o modelo é EM quando Bayesiano

--- R/plot_fit_on_data_em.R
+++ R/plot_fit_on_data_em.R
@@ -8,157 +8,88 @@
 #'
 #' @export
 plot_fit_on_data_em <- function(model, type = "survival") {
-  # check the object class
-  if (!inherits(model, "survival_ln_mixture_em")) {
-    stop("The object should be a survival_ln_mixture_em object.")
+  # Checks
+  if (!inherits(model, "survival_ln_mixture_em") &
+      !inherits(model, "survival_ln_mixture")) {
+    stop("The object should be a survival_ln_mixture_em or a survival_ln_mixture object.")
   }
-
-  # check type
+  
+  if(inherits(model, "survival_ln_mixture_em") & 
+     inherits(model, "survival_ln_mixture")) {
+    stop("The object should just one of two: survival_ln_mixture_em or survival_ln_mixture. Can't be both at the sime time.")
+  }
+  
   if (!type %in% c("survival", "hazard")) {
     stop("The type should be either 'survival' or 'hazard'.")
   }
-
-  form <- model$blueprint$formula
-  form <- stats::as.formula(paste0(paste(form)[[2]], " ~ ", paste(form)[[3]]))
-  vars <- strsplit(paste(form)[[3]], " \\+ ")[[1]]
-
-  data <- model$data
-
-  dict <- tibble::tibble(
-    key = NA,
-    match = NA,
-    category = NA
-  )
-
-  predictors <- data |>
-    dplyr::select(dplyr::all_of(vars))
-
-  time <- NA
-  estimate <- NA
-  .eval_time <- NA
-  .pred_survival <- NA
-  .pred_hazard <- NA
-
-  for (c in 1:ncol(predictors)) {
-    if (is.factor(predictors |>
-      dplyr::select(dplyr::all_of(c)) |>
-      dplyr::pull())) {
-      key <- names(predictors)[c]
-      var <- predictors |>
-        dplyr::select(dplyr::all_of(c)) |>
-        dplyr::pull()
-
-      for (i in levels(var)) {
-        match <- paste0(key, i)
-        category <- i
-        dict <- dplyr::bind_rows(dict, tibble::tibble(
-          key = key,
-          match = match,
-          category = category
-        ))
-      }
+  
+  # -----
+  # Predictions
+  
+  # which is the type of model fitted
+  if(inherits(model, "survival_ln_mixture_em")) {
+    data <- model$data
+    form <- model$blueprint$formula
+    form <- stats::as.formula(paste0(paste(form)[[2]], " ~ ", paste(form)[[3]]))
+    vars <- strsplit(paste(form)[[3]], " \\+ ")[[1]]
+    if(!model$blueprint$intercept) {
+      vars <- vars[vars != '0']
     }
-  }
-
-  dict <- dict |>
-    dplyr::slice(-1)
-
-  km <- survival::survfit(form, data) |>
-    broom::tidy()
-
-  eval_time <- seq(0, max(km$time))
-
-  preds <- stats::predict(model,
-    dplyr::distinct(data |>
-      dplyr::select(dplyr::all_of(vars))),
-    type = type,
-    eval_time = eval_time
-  ) |>
-    tidyr::unnest(.pred)
-
-  strata_preds <- rep(NA, nrow(preds))
-
-  for (i in 1:nrow(preds)) {
-    phrase <- NULL
-    next_index <- 0
-    for (c in 4:ncol(preds)) {
-      if (c < next_index) {
-        next
-      }
-
-      match <- names(preds)[c]
-      key_c <- dict$key[dict$match == match]
-      if (match %in% dict$match) {
-        dict_key <- dict |>
-          dplyr::filter(key == key_c)
-
-        if (nrow(dict_key) > 2) { # categorical covariate with more than 2 levels
-          category <- dict_key$category[!(dict_key$match %in% names(preds))]
-
-          for (j in c:ncol(preds)) {
-            match_j <- names(preds)[j]
-            key_j <- dict$key[dict$match == match_j]
-
-            if (key_j == key_c) {
-              if (preds[i, j] == 1) {
-                category <- dict_key$category[dict_key$match == match_j]
-              }
-            } else {
-              next_index <- j
-              break
-            }
-          }
-        } else {
-          if (preds[i, c] == 1) {
-            category <- dict_key$category[dict_key$match == match]
-          } else {
-            category <- dict_key$category[dict_key$match != match]
-          }
-        }
-
-        phrase <- paste0(phrase, key_c, "=", category)
-
-        if (c < ncol(preds)) {
-          phrase <- paste0(phrase, ", ")
-        }
+    
+    time <- NA
+    estimate <- NA
+    .eval_time <- NA
+    .pred_survival <- NA
+    .pred_hazard <- NA
+    
+    km <- survival::survfit(form, data) |>
+      broom::tidy()
+    
+    eval_time <- seq(0, max(km$time))
+    
+    if(all(vars != 'NULL')) {
+      preds <- stats::predict(model,
+                              dplyr::distinct(data |>
+                                                dplyr::select(dplyr::all_of(vars))),
+                              type = type,
+                              eval_time = eval_time
+      ) |>
+        tidyr::unnest(.pred)
+    } else {
+      preds <- stats::predict(model,
+                              data.frame(val = NA),
+                              type = type,
+                              eval_time = eval_time
+      ) |>
+        tidyr::unnest(.pred)
+    }
+    
+    
+    if(type == 'survival') {
+      if(all(vars != 'NULL')) {
+        gg <- ggplot() +
+          geom_step(aes(x = time, y = estimate, color = strata),
+                    data = km, alpha = 0.5) +
+          geom_line(aes(x = .eval_time, y = .pred_survival, color = strata),
+                    data = preds) +
+          theme_bw() +
+          labs(x = 'Time', y = 'Survival')
       } else {
-        phrase <- paste0(phrase, match, "=", preds[i, c])
-
-        if (c < ncol(preds)) {
-          phrase <- paste0(phrase, ", ")
-        }
+        gg <- ggplot() +
+          geom_step(aes(x = time, y = estimate),
+                    data = km, alpha = 0.5) +
+          geom_line(aes(x = .eval_time, y = .pred_survival),
+                    data = preds) +
+          theme_bw() +
+          labs(x = 'Time', y = 'Survival')
       }
+    } else {
+      # yet to be implemented hazard
     }
-    strata_preds[i] <- phrase
-  }
-
-  preds$strata <- strata_preds
-
-  if (type == "survival") {
-    gg <- ggplot() +
-      geom_step(aes(x = time, y = estimate, color = strata),
-        data = km,
-        alpha = 0.5
-      ) +
-      geom_line(aes(x = .eval_time, y = .pred_survival, color = strata), data = preds) +
-      theme_bw() +
-      labs(
-        x = "Time",
-        y = "Survival"
-      )
   } else {
-    gg <- ggplot() +
-      geom_step(aes(x = time, y = estimate, color = strata),
-        data = km,
-        alpha = 0.5
-      ) +
-      geom_line(aes(x = .eval_time, y = .pred_hazard, color = strata), data = preds) +
-      theme_bw() +
-      labs(
-        x = "Time",
-        y = "Survival"
-      )
+    # yet to be implemented bayesian
   }
-
-  return(gg)
+  
+  return(list(preds = preds,
+              ggplot = gg))
 }
